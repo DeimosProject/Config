15e2f669123e0eed203719492e001acba5a9644b
rez1dent3 <maksim.babichev95@gmail.com>
2016-12-23 15:02:17 +0300

    add object config

--- src/Deimos/Config/Config.php
+++ src/Deimos/Config/Config.php
@@ -2,9 +2,126 @@
 
 namespace Deimos\Config;
 
-class Config
+use Deimos\Builder\Builder;
+
+class Config extends \ArrayIterator
 {
 
+    /**
+     * @var string
+     */
+    protected $rootDir;
 
+    /**
+     * @var array
+     */
+    protected $configure = [];
+
+    /**
+     * @var Builder
+     */
+    protected $builder;
+
+    /**
+     * ConfigObject constructor.
+     *
+     * @param         $rootDir
+     * @param Builder $builder
+     */
+    public function __construct($rootDir, Builder $builder)
+    {
+        $this->rootDir = rtrim($rootDir, '/') . '/';
+        $this->builder = $builder;
+
+        parent::__construct();
+    }
+
+    /**
+     * @param $name
+     *
+     * @return array
+     */
+    protected function slice($name)
+    {
+        return explode(':', $name);
+    }
+
+    /**
+     * @param $name
+     *
+     * @return ConfigObject|mixed
+     */
+    public function __get($name)
+    {
+        return $this->get($name);
+    }
+
+    /**
+     * @param $name
+     * @param $value
+     *
+     * @throws \BadFunctionCallException
+     */
+    public function __set($name, $value)
+    {
+        throw new \BadFunctionCallException(__METHOD__);
+    }
+
+    /**
+     * @param $name
+     *
+     * @return bool
+     */
+    public function __isset($name)
+    {
+        return $this->exists($name);
+    }
+
+    /**
+     * @param $name
+     *
+     * @return bool
+     */
+    public function exists($name)
+    {
+        return file_exists($this->getPath($name));
+    }
+
+    /**
+     * @param $name string
+     *
+     * @return string
+     */
+    protected function getPath($name)
+    {
+        return $this->rootDir . 'config/' . $name . '.php';
+    }
+
+    /**
+     * @param $name
+     *
+     * @return ConfigObject|mixed
+     */
+    public function get($name)
+    {
+        list ($configName, $path) = $this->slice($name);
+
+        if (!isset($this->configure[$configName]))
+        {
+            $this->configure[$configName] = new ConfigObject($this->builder, $configName);
+        }
+
+        /**
+         * @var $configObject ConfigObject
+         */
+        $configObject = $this->configure[$configName];
+
+        if ($path !== null)
+        {
+            return $configObject->get($path);
+        }
+
+        return $configObject;
+    }
 
 }
---
+++ src/Deimos/Config/ConfigObject.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace Deimos\Config;
+
+use Deimos\Builder\Builder;
+use Deimos\Helper\Helper;
+
+class ConfigObject implements \Iterator
+{
+
+    /**
+     * @var string
+     */
+    protected $configPath;
+
+    /**
+     * @var bool
+     */
+    protected $loaded;
+
+    /**
+     * @var bool
+     */
+    protected $builder;
+
+    /**
+     * @var array
+     */
+    protected $storage = array();
+
+    /**
+     * ConfigObject constructor.
+     *
+     * @param Builder $builder
+     * @param string  $configPath
+     */
+    public function __construct($builder, $configPath)
+    {
+        $this->configPath = $configPath;
+        $this->builder    = $builder;
+        $this->loaded     = false;
+    }
+
+    /**
+     * @param $path string
+     *
+     * @return mixed
+     */
+    public function __get($path)
+    {
+        return $this->get($path);
+    }
+
+    /**
+     * @param $name
+     * @param $value
+     *
+     * @throws \BadFunctionCallException
+     */
+    public function __set($name, $value)
+    {
+        throw new \BadFunctionCallException(__METHOD__);
+    }
+
+    /**
+     * @param $name
+     *
+     * @return bool
+     */
+    public function __isset($name)
+    {
+        return $this->get($name) !== null;
+    }
+
+    /**
+     * @return Helper
+     */
+    public final function helper()
+    {
+        static $helper;
+
+        if (!$helper)
+        {
+            if (method_exists($this->builder, 'helper'))
+            {
+                $helper = $this->builder->helper();
+            }
+            else
+            {
+                $builder = $this->builder;
+                $helper  = new Helper($builder);
+            }
+        }
+
+        return $helper;
+    }
+
+    /**
+     * @param null|string $path
+     * @param mixed       $default
+     *
+     * @return mixed
+     */
+    public function get($path = null, $default = null)
+    {
+        return $this->helper()->arr()
+            ->get($this->storage, $path, $default);
+    }
+
+    /**
+     * Return the current element
+     *
+     * @link  http://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     * @since 5.0.0
+     */
+    public function current()
+    {
+        return current($this->storage);
+    }
+
+    /**
+     * Move forward to next element
+     *
+     * @link  http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function next()
+    {
+        next($this->storage);
+    }
+
+    /**
+     * Return the key of the current element
+     *
+     * @link  http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     * @since 5.0.0
+     */
+    public function key()
+    {
+        return key($this->storage);
+    }
+
+    /**
+     * Checks if current position is valid
+     *
+     * @link  http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     *        Returns true on success or false on failure.
+     * @since 5.0.0
+     */
+    public function valid()
+    {
+        return $this->current() !== false;
+    }
+
+    /**
+     * Rewind the Iterator to the first element
+     *
+     * @link  http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function rewind()
+    {
+        if (!$this->loaded)
+        {
+            $this->loaded  = true;
+            $this->storage = require $this->configPath;
+        }
+
+        reset($this->storage);
+    }
+
+}
